020f7a21a465a3bcc2ec690efebf3227b951d741
monktar <monktar@gmail.com>
2021-03-09 16:39:33 -0600

    Add files via upload

---
+++ extract schema of temp table.sql
@@ -0,0 +1,24 @@
+-- use to extract the schema of temp table
+
+SELECT * INTO #table1 FROM TableQuery
+
+
+SELECT char(9) + '[' + c.column_name + '] ' + c.data_type 
+   + CASE 
+        WHEN c.data_type IN ('decimal')
+            THEN isnull('(' + convert(varchar, c.numeric_precision) + ', ' + convert(varchar, c.numeric_scale) + ')', '') 
+        WHEN c.data_type IN ('varchar', 'nvarchar', 'char', 'nchar')
+            THEN isnull('(' 
+                + CASE WHEN c.character_maximum_length = -1
+                    THEN 'max'
+                    ELSE convert(varchar, c.character_maximum_length) 
+                  END + ')', '')
+        ELSE '' END
+   + CASE WHEN c.IS_NULLABLE = 'YES' THEN ' NULL' ELSE '' END
+   + ','
+FROM tempdb.INFORMATION_SCHEMA.COLUMNS c 
+WHERE TABLE_NAME LIKE '#table1%' 
+ORDER BY c.ordinal_position
+
+
+drop table #table1
