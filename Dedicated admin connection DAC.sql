1d8bb94235d18b7bd112ccbc6058d9feb911f008
monktar <monktar@gmail.com>
2021-11-26 18:30:29 -0600

    Add files via upload

--- Dedicated admin connection DAC.sql
+++ Dedicated admin connection DAC.sql
@@ -1,5 +1,9 @@
 -- https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/diagnostic-connection-for-database-administrators?view=sql-server-ver15
+--To establish a DAC from SQL Server Management Studio
 
+--Disconnect all connections to the related SQL Server instance, including the Object Explorer and all open query windows.
+--From the menu select File > New > Database Engine Query
+--From the connection dialog box in the Server Name field, enter admin:<server_name> if using the default instance or admin:<server_name>\<instance_name> if using a named instance.
 
 --enable
 sp_configure 'remote admin connections', 1;  
