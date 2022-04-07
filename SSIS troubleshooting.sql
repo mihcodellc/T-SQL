682440f5c7cfbf5406f46b3e7371bca933308aaf
monktar <monktar@gmail.com>
2022-04-07 09:06:57 -0500

    Add files via upload

--- SSIS troubleshooting.sql
+++ SSIS troubleshooting.sql
@@ -1,12 +1,11 @@
--- you can still use the report(all executions and drill down or filter) on ssisdb under IS Catalogs
+-- you can still use the report(all executions and drill down or filter) on ssisdb
 -- OR 
---change the @mydate and 
--- replace execution_id on 2nd table by the one to investigate from table 1 
+--change the @mydate using the first query 
 --then on the 4th table use the event_message_id on 3rd - 3
-declare @mydate datetime = '20220301'
+declare @mydate datetime = '20220407'
 
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT distinct inf.[execution_id]
+/****** info if no rows ie succeed ******/
+SELECT  inf.[execution_id]
       ,[folder_name]
       ,[project_name]
       ,inf.[package_name]
@@ -19,15 +18,15 @@ SELECT distinct inf.[execution_id]
       ,[process_id]
       ,[stopped_by_name]
   FROM SSISDB.internal.execution_info inf
-  join [SSISDB].[catalog].[executable_statistics] stat 
+  join [SSISDB].catalog.executable_statistics stat 
     on inf.execution_id = stat.execution_id
-  where folder_name = 'merlin' and inf.start_time  > @mydate --and inf.start_time  < '20220317' --and  inf.execution_id  = 1772073
-  and stat.execution_result in (1,2,3) -- 1 Failure,  2 Completion 3 Cancelled
+  where folder_name = 'merlin' and inf.start_time between @mydate and dateadd(dd,1,@mydate) 
+  and stat.execution_result in (1,2,3) -- 1 Failure,  2 Completion 3 Cancelled, 0 Success
   --convert(DATETIME, convert(CHAR(10), GETDATE(), 110), 110)
 
 
-  /****** packages steps  ******/
-SELECT TOP (1000) [statistics_id]
+  /****** statistics  ******/
+SELECT [statistics_id]
       ,[execution_id]
       ,[executable_id]
       ,[execution_path]
@@ -36,50 +35,32 @@ SELECT TOP (1000) [statistics_id]
       ,[execution_duration]
       ,[execution_result]
       ,[execution_value]
-  FROM [SSISDB].[catalog].[executable_statistics]
-  where start_time > @mydate and execution_id  = 1772004
+  FROM [SSISDB].[catalog].[executable_statistics] st
+  where start_time between @mydate and dateadd(dd,1,@mydate)
+	   and exists (select 1 from SSISDB.internal.execution_info inf where inf.execution_id  = st.execution_id)
 
-
-  --- packages events
-  SELECT [event_message_id]
+ /****** event_messages  ******/
+  SELECT  [event_name]
+       ,[package_name]
+       ,[message_source_name]
+	  ,[execution_path]
       ,[operation_id]
       ,[message_time]
       ,[message_type]
       ,[message_source_type]
       ,[message]
       ,[extended_info_id]
-      ,[package_name]
-      ,[event_name]
-      ,[message_source_name]
       ,[message_source_id]
       ,[subcomponent_name]
       ,[package_path]
-      ,[execution_path]
       ,[threadID]
       ,[message_code]
       ,[event_message_guid]
-  FROM [SSISDB].[catalog].[event_messages]
-   where  operation_id in(1772004)
-   and event_name in ('OnTaskFailed')
+	 ,[event_message_id]
+  FROM [SSISDB].[catalog].[event_messages] msg
+   where  
+   event_name in ('OnTaskFailed', 'OnError', 'OnWarning') 
+   and  
+   exists (select 1 from SSISDB.internal.execution_info inf 
+		  where inf.execution_id  = msg.operation_id and inf.start_time between @mydate and dateadd(dd,1,@mydate))
 
-
-   --packages events prior to the fail
-   SELECT [event_message_id]
-      ,[operation_id]
-      ,[message_time]
-      ,[message_type]
-      ,[message_source_type]
-      ,[message]
-      ,[extended_info_id]
-      ,[package_name]
-      ,[event_name]
-      ,[message_source_name]
-      ,[message_source_id]
-      ,[subcomponent_name]
-      ,[package_path]
-      ,[execution_path]
-      ,[threadID]
-      ,[message_code]
-      ,[event_message_guid]
-  FROM [SSISDB].[catalog].[event_messages]
-   where  event_message_id > 124576172 and operation_id in(1772004)
