df1d5c120fe8754e249edd8a55b2c98b9b47948d
monktar <monktar@gmail.com>
2022-04-22 15:55:08 -0500

    Update performance pressure.sql

--- performance pressure.sql
+++ performance pressure.sql
@@ -2,10 +2,10 @@
 
 -- SQL SERVER CONFIGURATION MANAGER to activate your sql agent or start db engine ...
 
--- SQL News
-https://cloudblogs.microsoft.com/sqlserver/?product%5B%5D=azure-data-lake&product%5B%5D=azure-data-studio&product%5B%5D=azure-sql-database&product%5B%5D=sql-server-2012&product%5B%5D=sql-server-2016&product%5B%5D=sql-server-2017&product%5B%5D=sql-server-2019&product%5B%5D=sql-server-2022
+--*** SQL News
+-- https://cloudblogs.microsoft.com/sqlserver/?product%5B%5D=azure-data-lake&product%5B%5D=azure-data-studio&product%5B%5D=azure-sql-database&product%5B%5D=sql-server-2012&product%5B%5D=sql-server-2016&product%5B%5D=sql-server-2017&product%5B%5D=sql-server-2019&product%5B%5D=sql-server-2022
 
---Cumulative updates
+--*** Cumulative updates
 https://sqlserverupdates.com/
 https://www.brentozar.com/archive/2021/06/how-to-patch-sql-server/
 
