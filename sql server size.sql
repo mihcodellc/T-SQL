a1c19f3d90f12a3a81d147db32d2f9a3b0ef4a7b
monktar <monktar@gmail.com>
2022-05-26 11:57:46 -0500

    Add files via upload

--- sql server size.sql
+++ sql server size.sql
@@ -86,33 +86,55 @@ select ' current db file info'
 ----exec sp_helpfile;
 ----'
 -- https://dba.stackexchange.com/questions/7917/how-to-determine-used-free-space-within-sql-database-files/7921#7921
+ declare @clause nvarchar(2000)
+
+ set @clause = '
+	   use [?]
 SELECT 
     [TYPE] = A.TYPE_DESC
     ,[FILE_Name] = A.name
     ,[FILEGROUP_NAME] = fg.name
     ,[File_Location] = A.PHYSICAL_NAME
     ,[FILESIZE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0)
-    ,[USEDSPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - ((SIZE/128.0) - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0))
-    ,[FREESPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0)
-    ,[FREESPACE_%] = CONVERT(DECIMAL(10,2),((A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0)/(A.SIZE/128.0))*100)
-    ,[AutoGrow] = 'By ' + CASE is_percent_growth WHEN 0 THEN CAST(growth/128 AS VARCHAR(10)) + ' MB -' 
-        WHEN 1 THEN CAST(growth AS VARCHAR(10)) + '% -' ELSE '' END 
-        + CASE max_size WHEN 0 THEN 'DISABLED' WHEN -1 THEN ' Unrestricted' 
-            ELSE ' Restricted to ' + CAST(max_size/(128*1024) AS VARCHAR(10)) + ' GB' END 
-        + CASE is_percent_growth WHEN 1 THEN ' [autogrowth by percent, BAD setting!]' ELSE '' END
+    ,[USEDSPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - ((SIZE/128.0) - CAST(FILEPROPERTY(A.NAME, ''SPACEUSED'') AS INT)/128.0))
+    ,[FREESPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, ''SPACEUSED'') AS INT)/128.0)
+    ,[FREESPACE_%] = CONVERT(DECIMAL(10,2),((A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, ''SPACEUSED'') AS INT)/128.0)/(A.SIZE/128.0))*100)
+    ,[AutoGrow] = ''By '' + CASE is_percent_growth WHEN 0 THEN CAST(growth/128 AS VARCHAR(10)) + '' MB -'' 
+        WHEN 1 THEN CAST(growth AS VARCHAR(10)) + ''% -'' ELSE '''' END 
+        + CASE max_size WHEN 0 THEN ''DISABLED'' WHEN -1 THEN '' Unrestricted'' 
+            ELSE '' Restricted to '' + CAST(max_size/(128*1024) AS VARCHAR(10)) + '' GB'' END 
+        + CASE is_percent_growth WHEN 1 THEN '' [autogrowth by percent, BAD setting!]'' ELSE '''' END
 FROM sys.database_files A LEFT JOIN sys.filegroups fg ON A.data_space_id = fg.data_space_id 
 order by A.TYPE desc, A.NAME;
+	   '
+	    exec sp_MSforeachdb @clause
+--SELECT 
+--    [TYPE] = A.TYPE_DESC
+--    ,[FILE_Name] = A.name
+--    ,[FILEGROUP_NAME] = fg.name
+--    ,[File_Location] = A.PHYSICAL_NAME
+--    ,[FILESIZE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0)
+--    ,[USEDSPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - ((SIZE/128.0) - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0))
+--    ,[FREESPACE_MB] = CONVERT(DECIMAL(10,2),A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0)
+--    ,[FREESPACE_%] = CONVERT(DECIMAL(10,2),((A.SIZE/128.0 - CAST(FILEPROPERTY(A.NAME, 'SPACEUSED') AS INT)/128.0)/(A.SIZE/128.0))*100)
+--    ,[AutoGrow] = 'By ' + CASE is_percent_growth WHEN 0 THEN CAST(growth/128 AS VARCHAR(10)) + ' MB -' 
+--        WHEN 1 THEN CAST(growth AS VARCHAR(10)) + '% -' ELSE '' END 
+--        + CASE max_size WHEN 0 THEN 'DISABLED' WHEN -1 THEN ' Unrestricted' 
+--            ELSE ' Restricted to ' + CAST(max_size/(128*1024) AS VARCHAR(10)) + ' GB' END 
+--        + CASE is_percent_growth WHEN 1 THEN ' [autogrowth by percent, BAD setting!]' ELSE '' END
+--FROM sys.database_files A LEFT JOIN sys.filegroups fg ON A.data_space_id = fg.data_space_id 
+--order by A.TYPE desc, A.NAME;
 
+ 
 
-
-SELECT a.object_id, object_name(a.object_id) AS TableName,
-    a.index_id, b.name AS IndedxName, avg_fragmentation_in_percent,b.type_desc, b.fill_factor,   b.is_disabled
-FROM sys.dm_db_index_physical_stats (DB_ID (db_name()) , NULL, NULL, NULL, NULL) AS a
-INNER JOIN sys.indexes AS b
-    ON a.object_id = b.object_id
-    AND a.index_id = b.index_id
-where avg_fragmentation_in_percent > 80 and b.name is not null
-order by avg_fragmentation_in_percent desc
+--SELECT a.object_id, object_name(a.object_id) AS TableName,
+--    a.index_id, b.name AS IndedxName, avg_fragmentation_in_percent,b.type_desc, b.fill_factor,   b.is_disabled
+--FROM sys.dm_db_index_physical_stats (DB_ID (db_name()) , NULL, NULL, NULL, NULL) AS a
+--INNER JOIN sys.indexes AS b
+--    ON a.object_id = b.object_id
+--    AND a.index_id = b.index_id
+--where avg_fragmentation_in_percent > 80 and b.name is not null
+--order by avg_fragmentation_in_percent desc
 
 ----RmsAdmin
 --object_id	TableName	index_id	IndedxName	avg_fragmentation_in_percent	type_desc	fill_factor	is_disabled
