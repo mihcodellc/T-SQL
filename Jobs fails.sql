a4bebcd6823d287bdba44bc58d2a3a610e01533f
monktar <monktar@gmail.com>
2022-07-01 17:01:03 -0500

    Update Jobs fails.sql

--- Jobs fails.sql
+++ Jobs fails.sql
@@ -1,6 +1,8 @@
 --  fail jobs based on scheduled period covering all your current jobs
 --must worry when table 2 is not empty
 
+
+
 --1 
   declare @date1 int, @date2 int;
     set @date1 = convert(int,replace(convert(CHAR(10), GETDATE(), 112),'-','')) --20220316
@@ -43,24 +45,15 @@
 
    
 
-  select '********************DBA: DR_BackupLogins************************'
+ ---- https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-help-job-transact-sql?view=sql-server-ver16
+---- session running included active job will show on Connected 3.0.sql beneath the label: select 'sessions running but asleep'
 
-  SELECT job.name, his.step_name, his.run_status , his.run_time, his.run_date, 
-			CASE WHEN his.run_status = 1 THEN 'Success'  
-		     WHEN his.run_status = 0 THEN 'Failure'
-			WHEN his.run_status = 2 THEN 'Retried'
-			WHEN his.run_status = 3 THEN 'Cancelled'
-			WHEN his.run_status = 4 THEN 'Check its steps'
-			ELSE 'Unknown' END   AS outcome_message,  
-			his.run_duration as 'Duration HHMMSS', his.[message],
- his.sql_severity, his.retries_attempted, job.job_id 
-	    , job.notify_email_operator_id,  his.[server]
-  FROM [msdb].[dbo].[sysjobhistory] as his
-  JOIN [msdb].[dbo].[sysjobs] as job on job.job_id = his.job_id
-  WHERE run_date between @date2  and  @date1
-  AND job.name = 'DBA: DR_BackupLogins' --1 Succeeded (0/*FAILED*/,2/*RETRY*/, 3/*CANCELED*/, 4/*In Progress*/, 5 /*unknown*/) --1 Succeeded
-  order by run_date desc, run_time desc
+select '*******a job info and status : uncomment the lines below **************'
+---- job info -- current status column named current_execution_status
+--exec msdb.dbo.sp_help_job @job_name= 'Billing : Monthly_Invoices' 
 
+---- job Executing
+--exec msdb.dbo.sp_help_job @execution_status=1 --- running
 
 
 
