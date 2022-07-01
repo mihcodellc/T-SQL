adf7efb5d26d20c48b63207fc30eae485c26f000
monktar <monktar@gmail.com>
2022-07-01 15:26:02 -0500

    Update pass data in jobs step.sql

--- pass data in jobs step.sql
+++ pass data in jobs step.sql
@@ -1,12 +1,15 @@
+--inspired by https://www.mssqltips.com/sqlservertip/5731/how-to-pass-data-between-sql-server-agent-job-steps/
+USE [msdb]
+GO
 
 USE [msdb]
 GO
 
-/****** Object:  Job [DoProceed]    Script Date: 7/1/2022 11:01:53 AM ******/
+/****** Object:  Job [DoProceed]    Script Date: 7/1/2022 3:25:12 PM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 7/1/2022 11:01:53 AM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 7/1/2022 3:25:13 PM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -16,7 +19,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DoProceed', 
-		@enabled=1, 
+		@enabled=0, 
 		@notify_level_eventlog=0, 
 		@notify_level_email=0, 
 		@notify_level_netsend=0, 
@@ -29,7 +32,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DoProceed',
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'mbello', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [check period]    Script Date: 7/1/2022 11:01:54 AM ******/
+/****** Object:  Step [check period]    Script Date: 7/1/2022 3:25:13 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'check period', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -40,22 +43,54 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'check pe
 		@retry_attempts=0, 
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'declare @proceed nchar(2) 
+		@command=N'declare @proceed nchar(3) 
 
 set @proceed = N''No''
 
 if DATEPART(dd, getdate()) between 1 and 4
     set @proceed = N''Yes''
 
-raiserror (''@proceed = %s'', 10, 1, @proceed) with log;
+raiserror (''Notification @proceed = %s'', 10, 1, @proceed) with log;
 
 ', 
 		@database_name=N'master', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Proceed]    Script Date: 7/1/2022 11:01:54 AM ******/
+/****** Object:  Step [Proceed]    Script Date: 7/1/2022 3:25:13 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Proceed', 
 		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'
+set nocount on;
+declare @t table (LogDate datetime, ProcessInfo varchar(100), [text] varchar(300));
+
+insert into @t (LogDate, ProcessInfo, [Text])
+exec master.sys.sp_readerrorlog 0, 1, ''proceed'';
+
+declare @proceed nchar(3)  
+
+select top 1 @proceed = substring([text], charindex(''@proceed = '', [text])+len(''@proceed = '')+1, 128)
+from @t
+order by LogDate desc;
+
+if  @proceed = ''Yes''
+     EXECUTE msdb..sp_send_dbmail  @Profile_Name = ''DataServicesProfile'' , @From_Address = ''NoReply@revmansolutions.com'',  @Recipients = ''mbello@rmsweb.com'',  @BODY = ''Ready to proceed ?''
+
+
+', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [reset the last top 1]    Script Date: 7/1/2022 3:25:13 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'reset the last top 1', 
+		@step_id=3, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
@@ -64,34 +99,11 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Proceed'
 		@retry_attempts=0, 
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'set nocount on;
-declare @t table (LogDate datetime, ProcessInfo varchar(100), [text] varchar(300));
+		@command=N'declare @proceed nchar(3) 
 
-insert into @t (LogDate, ProcessInfo, [Text])
-exec master.sys.sp_readerrorlog 0, 1, ''proceed'';
-
-declare @proceed nchar(2)  
-
-select top 1 @proceed = substring([text], charindex(''@proceed = '', [text])+len(''@proceed = '')+1, 128)
-from @t
-order by LogDate desc;
-
-set nocount on;
-declare @t table (LogDate datetime, ProcessInfo varchar(100), [text] varchar(300));
-
-insert into @t (LogDate, ProcessInfo, [Text])
-exec master.sys.sp_readerrorlog 0, 1, ''proceed'';
-
-declare @proceed nchar(2)  
-
-select top 1 @proceed = substring([text], charindex(''@proceed = '', [text])+len(''@proceed = '')+1, 128)
-from @t
-order by LogDate desc;
-
-if  @proceed = ''Yes''
-     EXECUTE msdb..sp_send_dbmail  @Profile_Name = ''MyProfile'' , @From_Address = ''NoReply@mitiri.com'',  @Recipients = ''mbello@mitiri.com'',  @BODY = ''Ready to proceed ?''
-
-
+set @proceed = N''No''
+--the previous define Yes this one will create a row with No hidnf the Yes to be the top 1 at step 2
+raiserror (''Notification @proceed = %s'', 10, 1, @proceed) with log;
 ', 
 		@database_name=N'master', 
 		@flags=0
@@ -103,7 +115,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'@ 9am',
 		@freq_type=4, 
 		@freq_interval=1, 
 		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
+		@freq_subday_interval=1, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
 		@active_start_date=20220701, 
@@ -120,5 +132,6 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
+
 
 
