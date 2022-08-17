74a86c3efa646483d7c322ee5808fd407f43bb7d
monktar <monktar@gmail.com>
2022-08-17 11:27:28 -0500

    Add files via upload

---
+++ Table_file_group.sql
@@ -0,0 +1,14 @@
+-- where are my tables
+SELECT OBJECT_NAME([si].[object_id]) AS [tablename]
+    ,[ds].[name] AS [filegroupname]
+    ,[df].[physical_name] AS [datafilename]
+    , df.name as FileLogicalName
+FROM [sys].[data_spaces] [ds]
+--Contains a row per file of a database as stored = [database_files]
+INNER JOIN [sys].[database_files] [df] ON [ds].[data_space_id] = [df].[data_space_id]
+INNER JOIN [sys].[indexes] [si] ON [si].[data_space_id] = [ds].[data_space_id]
+    AND [si].[index_id] < 2
+INNER JOIN [sys].[objects] [so] ON [si].[object_id] = [so].[object_id]
+WHERE [so].[type] = 'U'
+    AND [so].[is_ms_shipped] = 0
+ORDER BY [tablename] ASC;
