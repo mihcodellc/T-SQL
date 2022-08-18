97f1123a0dc85dcdfd7d89b722cba62171b8a0bf
monktar <monktar@gmail.com>
2022-08-18 12:17:30 -0500

    Update FileStream partition.sql

--- FileStream partition.sql
+++ FileStream partition.sql
@@ -20,6 +20,22 @@ FROM sys.partitions
 WHERE OBJECT_NAME(OBJECT_ID)='Orders_P' -- source table
 GO
 
+	-- where are my tables
+SELECT OBJECT_NAME([si].[object_id]) AS [tablename]
+    ,[ds].[name] AS [filegroupname]
+    ,[df].[physical_name] AS [datafilename]
+    , df.name as FileLogicalName
+FROM [sys].[data_spaces] [ds]
+--Contains a row per file of a database as stored = [database_files]
+INNER JOIN [sys].[database_files] [df] ON [ds].[data_space_id] = [df].[data_space_id]
+INNER JOIN [sys].[indexes] [si] ON [si].[data_space_id] = [ds].[data_space_id]
+    AND [si].[index_id] < 2
+INNER JOIN [sys].[objects] [so] ON [si].[object_id] = [so].[object_id]
+WHERE [so].[type] = 'U' and OBJECT_NAME([si].[object_id]) ='Orders'
+    AND [so].[is_ms_shipped] = 0
+ORDER BY [tablename] ASC;
+
+
 SELECT partition_id, index_id, partition_number, Rows, OBJECT_NAME(OBJECT_ID) 
 FROM sys.partitions
 WHERE OBJECT_NAME(OBJECT_ID)='Orders' -- target table
