96627a892c72980e4781c20ed54996b0455c7c4f
monktar <monktar@gmail.com>
2022-08-31 11:53:18 -0500

    Create On Call Investigation.sql

---
+++ On Call Investigation.sql
@@ -0,0 +1,8 @@
+A full description of the issue in a DB Ticket
+steps to replicate the issue without it there is no way to fix it
+what the normal flow(nominal scenario) looks like in this situation; That way, I know when it is no longer an issue
+what steps has already been taken to FIND the root cause of the issue
+what steps has already been taken to FIX the root cause of the issue
+My point of contact who can answer my questions 
+create a group chat with all involved in this matter
+does this issue is user( or server, application ...) specific or not? if yes, what is user(or server or application ...) used?
