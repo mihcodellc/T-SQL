2230fbbea2f10e6500469e750c4fa6daf43210d3
monktar <monktar@gmail.com>
2022-09-08 15:33:09 -0500

    Update OLA backup.sql

--- OLA backup.sql
+++ OLA backup.sql
@@ -18,3 +18,23 @@ BACKUP DATABASE TestBello TO DISK = 'C:\Backups\TestBello.DIF' WITH DIFFERENTIAL
 BACKUP LOG TestBello TO DISK = 'C:\Backups\TestBello.TRN' WITH COMPRESSION, CHECKSUM --LOG
 
 --ALTER DATABASE [TestBello] SET RECOVERY FULL
+
+
+-- interesting part is the @BlockSize which induce error some operations of sql server
+-- incrementation, increase of storage, drive, disk happens here by size of 64K ie 65536B
+-- need to research more on effect of blocksize
+
+EXECUTE [dbo].[DatabaseBackup]
+@Databases = 'USER_DATABASES',
+@Directory = N'G:\MSSQL\Backup',
+@BackupType = 'FULL',
+
+@BufferCount = 50,
+@MaxTransferSize = 4194304,
+@BlockSize = 65536,  -- 512,1024,2048,4096,8192,16384,32768,6553
+@NumberOfFiles = 16,
+
+@Verify = 'Y',
+@CleanupTime = 336,
+@CheckSum = 'Y',
+@LogToTable = 'Y'
