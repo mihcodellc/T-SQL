1387850073bfc0c07fcb292e4a318abf4ebcb327
monktar <monktar@gmail.com>
2022-09-26 16:29:44 -0500

    Create prevent cross database query.sql

---
+++ prevent cross database query.sql
@@ -0,0 +1,4 @@
+https://www.mssqltips.com/sqlservertip/2549/options-for-cross-database-access-within-sql-server/
+
+--1. Enabling the cross database ownership chaining.
+--2. Setting the first database as TRUSTWORTHY and granting its owner AUTHENTICATE in the second database.
