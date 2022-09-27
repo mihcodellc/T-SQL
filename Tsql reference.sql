59f8451df00fffec291b96dc1323ee3b0be2149a
monktar <monktar@gmail.com>
2022-09-27 10:47:42 -0500

    Create Tsql reference.sql

---
+++ Tsql reference.sql
@@ -0,0 +1,3 @@
+always refer to update your knownledge
+here is the link to TSQL Functions Reference
+https://learn.microsoft.com/en-us/sql/relational-databases/system-functions/system-functions-category-transact-sql?view=sql-server-ver16
