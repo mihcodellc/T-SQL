75e87b87f0c79c7b1849d9e2f46e61fb655ba736
monktar <monktar@gmail.com>
2022-09-27 23:19:27 -0500

    Add files via upload

---
+++ tsql update.sql
@@ -0,0 +1,15 @@
+temporal table(system versioning) vs hist table usage on performance
+for system_time as for
+convert to temporal table 
+https://learn.microsoft.com/en-us/sql/relational-databases/tables/temporal-tables?view=sql-server-ver16
+https://learn.microsoft.com/en-us/sql/relational-databases/tables/querying-data-in-a-system-versioned-temporal-table?view=sql-server-ver16
+
+recursive common table 
+https://learn.microsoft.com/en-us/sql/t-sql/queries/with-common-table-expression-transact-sql?view=sql-server-ver16
+
+
+feature between standard and interprise edtion
+treshold of parllelism from 2016 to 2022
+Core Licence min in standard compare to enterprise
+deprecated data types: money?
+cursor forward read only VERSUS while loop
