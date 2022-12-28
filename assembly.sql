2c7714183ac5a8a7b40688ba432476060e48fba1
monktar <monktar@gmail.com>
2022-12-28 14:04:43 -0600

    Update and rename assembly to assembly.sql

--- assembly.sql
+++ assembly.sql
@@ -1,17 +1,18 @@
 -- https://docs.microsoft.com/en-us/troubleshoot/sql/admin/error-run-clr-object-create-assembly
 --An error occurred in the Microsoft .NET Framework while trying to load assembly id ...
 
+--1
+ALTER DATABASE <user_db_name> SET TRUSTWORTHY ON;
+--2
+USE <user_db_name>;
+EXEC sp_changedbowner 'sa' --the original owner db owner 
+
+
 USE <user_db_name>;
 
 EXEC sp_configure 'clr enabled', 1;  
 RECONFIGURE;  
 GO 
-
-
-ALTER DATABASE <user_db_name> SET TRUSTWORTHY ON;
-
-USE <user_db_name>;
-EXEC sp_changedbowner 'sa' --the original owner db owner 
 
 
 --map user in the database
