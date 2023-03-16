def9303b1d5c6119217beba247873167475281ba
monktar <monktar@gmail.com>
2023-03-16 17:14:02 -0500

    Update Azure SQL.sql

--- Azure SQL.sql
+++ Azure SQL.sql
@@ -1,7 +1,21 @@
 --details space used
-EXEC sp_spaceused @oneresultset = 1
+declare @t table (database_name nvarchar(128), database_Data_Log nvarchar(128), unallocated_space nvarchar(128), reserved nvarchar(128), 
+data nvarchar(128), index_size nvarchar(128), unused nvarchar(128))
+
+insert into @t
+EXEC sp_spaceused @oneresultset = 1 
+select database_name,   
+cast(substring(database_Data_Log,0,CHARINDEX(' ', database_Data_Log)) as float) [database_Data_Log_MB = CurrentSizeOnDisk],
+convert(float,(substring(unallocated_space,0,CHARINDEX(' ', unallocated_space)))) unallocated_MB,
+cast(substring(reserved,0,CHARINDEX(' ', reserved)) as float)/1000 [reserved_MB /*= data + Index + Unused*/],
+cast(substring(data,0,CHARINDEX(' ', data)) as float)/1000 data_MB,
+convert(float,(substring(index_size,0,CHARINDEX(' ', index_size))))/1000 index_size_MB,
+cast(substring(unused,0,CHARINDEX(' ', unused)) as float)/1000 unused_MB, GETDATE() as Whe_n 
+ from @t
+ order by [database_Data_Log_MB = CurrentSizeOnDisk] desc
 
 
+--db property
 SELECT Edition = DATABASEPROPERTYEX('MyDatabase', 'EDITION'),
         ServiceObjective = DATABASEPROPERTYEX('MyDatabase', 'ServiceObjective'),
         MaxSizeInBytes =  DATABASEPROPERTYEX('MyDatabase', 'MaxSizeInBytes');
