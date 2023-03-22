499cab8270ceb17b6d05b2a196d214ce57672c5a
monktar <monktar@gmail.com>
2023-03-22 16:33:35 -0500

    Update MoveTable_To_FileGroup delete.sql

--- MoveTable_To_FileGroup delete.sql
+++ MoveTable_To_FileGroup delete.sql
@@ -3,8 +3,35 @@
 
 -- delete
 -- https://www.mssqltips.com/sqlservertip/5921/delete-data-from-large-sql-server-tables-with-minimal-logging/
+--0- log size 
+use Maintenance
+IF NOT EXISTS (SELECT * FROM sys.objects
+WHERE object_id = OBJECT_ID(N'[dbo].[dbSize_datasplit]') AND type in (N'U'))
+begin
+CREATE TABLE [dbo].[dbSize_datasplit](
+	[name] [varchar](128) NULL,
+	[AvailableSpaceInMB] [bigint] NULL,
+	[UsedSpaceMB] [bigint] NULL,
+	[OriginalSizeMB] [bigint] NULL,
+	[TimeChecked] [datetime] NULL
+) ON [PRIMARY]
+end
+go
 
---1-add file group
+Use MyDB
+Insert into maintenance.dbo.dbSize_datasplit
+SELECT name ,size/128.0 - CAST(FILEPROPERTY(name, 'SpaceUsed') AS int)/128.0 AS AvailableSpaceInMB, CAST(FILEPROPERTY(name, 'SpaceUsed') AS int)/128.0 as UsedSpace,
+size/128.0 AS OriginalSizeMB, getdate()
+FROM sys.database_files
+where type <> 1 -- exclude log
+--and name like 'MyDb_Data%' 
+go
+
+--1- change the recovery model
+ALTER DATABASE MedRx SET RECOVERY SIMPLE;
+go
+
+--2-add file group
 ALTER DATABASE WideWorldImporters ADD FILEGROUP [FG_Orders]
 GO
 
@@ -24,7 +51,7 @@ GO
 SET STATISTICS PROFILE ON
 GO
 
---Moving table with a clustered index then move the nc clustered indexes
+--3-Moving data by rebuild PK  a clustered index then rebuild the nc clustered indexes
 -- ************PK and others indexes
 -- ************make sure to use : DROP_EXISTING = ON, ONLINE = ON both for NC -- DROP_EXISTING = ON only for PK
 --*************replace [FG_Orders] by the new Filegroup
@@ -35,3 +62,18 @@ GO
 --Because the leaf level of a clustered index and the data pages are the same by definition, creating a clustered index and using the ON partition_scheme_name or ON filegroup_name clause effectively moves a table from the filegroup on which the table was created to the new partition scheme or filegroup. Before creating tables or indexes on specific filegroups, verify which filegroups are available and that they have enough empty space for the index.
 CREATE UNIQUE CLUSTERED INDEX PK_Sales_Orders ON Sales.Orders (OrderID)
 	WITH (DROP_EXISTING = ON, ONLINE = ON, FILLFACTOR = 90) ON [FG_Orders]
+
+
+--4- log size at the end
+Use MyDB
+Insert into maintenance.dbo.dbSize_datasplit
+SELECT name ,size/128.0 - CAST(FILEPROPERTY(name, 'SpaceUsed') AS int)/128.0 AS AvailableSpaceInMB, CAST(FILEPROPERTY(name, 'SpaceUsed') AS int)/128.0 as UsedSpace,
+size/128.0 AS OriginalSizeMB, getdate()
+FROM sys.database_files
+where type <> 1 -- exclude log
+--and name like 'MyDb_Data%' 
+go
+
+
+--5-put back db to full recovery
+ALTER DATABASE MedRx  SET RECOVERY full; 
