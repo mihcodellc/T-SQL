680686c0307fa577af445feefe6cafd5b058d068
monktar <monktar@gmail.com>
2023-04-17 12:40:15 -0500

    Update LOG LSN in backup.sql

--- LOG LSN in backup.sql
+++ LOG LSN in backup.sql
@@ -93,6 +93,34 @@ end
 -- https://blog.sqlauthority.com/2015/08/10/sql-server-error-msg-4305-level-16-state-1-the-log-in-this-backup-set-terminates-at-lsn-which-is-too-early-to-apply-to-the-database/
 -- https://www.mssqltips.com/sqlservertip/3209/understanding-sql-server-log-sequence-numbers-for-backups/
 
+
+--db backups overview
+-- Assign the database name to variable below
+DECLARE @db_name VARCHAR(100)
+SELECT @db_name = 'Archive'
+-- query
+SELECT TOP (30) s.database_name
+,m.physical_device_name
+,CAST(CAST(s.backup_size / 1000000 AS INT) AS VARCHAR(14)) + ' ' + 'MB' AS bkSize
+,CAST(DATEDIFF(second, s.backup_start_date, s.backup_finish_date) AS VARCHAR(4)) + ' ' + 'Seconds' TimeTaken
+,s.backup_start_date
+,CAST(s.first_lsn AS VARCHAR(50)) AS first_lsn
+,CAST(s.last_lsn AS VARCHAR(50)) AS last_lsn
+,CASE s.[type] WHEN 'D'
+THEN 'Full'
+WHEN 'I'
+THEN 'Differential'
+WHEN 'L'
+THEN 'Transaction Log'
+END AS BackupType
+,s.server_name
+,s.recovery_model
+FROM msdb.dbo.backupset s
+INNER JOIN msdb.dbo.backupmediafamily m ON s.media_set_id = m.media_set_id
+WHERE s.database_name = @db_name
+ORDER BY backup_start_date DESC
+,backup_finish_date
+
 --get full info knowing physical_device_name
 select l.backup_set_id as Logbackup_set_id, l.backup_start_date , m.physical_device_name as FirstLogBackUp, l.checkpoint_lsn 
 FROM msdb.dbo.backupset l 
