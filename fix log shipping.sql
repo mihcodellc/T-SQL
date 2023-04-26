516721f072aa07d4c1c71b9bdab78698b49eb70f
monktar <monktar@gmail.com>
2023-04-26 10:49:37 -0500

    Update fix log shipping.sql

--- fix log shipping.sql
+++ fix log shipping.sql
@@ -11,7 +11,7 @@
   select * from [msdb].[dbo].[log_shipping_secondary_databases] 
 --2-b replace secondary_database and secondary_id in below insert accordingly to step 1  
 --insert into msdb.dbo.log_shipping_secondary_databases
--- SELECT 'RMSOCR'
+-- SELECT 'mydb_'
 --      ,'5703DFAD-35AA-4FD3-A5BB-B29179F70E2C' 
 --      ,[restore_delay]
 --      ,[restore_all]
@@ -23,7 +23,7 @@
 --      ,[last_restored_file]
 --      ,[last_restored_date]
 --  FROM [msdb].[dbo].[log_shipping_secondary_databases]
---   where secondary_database ='reconciliation'
+--   where secondary_database ='mydb_good_shipped'
 
   
 
@@ -32,10 +32,10 @@
 --3-b replace secondary_database and secondary_id, and primary database in below insert accordingly to step 1  
 --insert into [msdb].[dbo].[log_shipping_monitor_secondary]
 --SELECT [secondary_server]
---      ,'RMSOCR'
+--      ,'mydb_'
 --      ,'5703DFAD-35AA-4FD3-A5BB-B29179F70E2C'
 --      ,[primary_server]
---      ,'RMSOCR'
+--      ,'mydb_'
 --      ,[restore_threshold]
 --      ,[threshold_alert]
 --      ,[threshold_alert_enabled]
@@ -48,7 +48,7 @@
 --      ,[last_restored_latency]
 --      ,[history_retention_period]
 --  FROM [msdb].[dbo].[log_shipping_monitor_secondary]
---  where secondary_database = 'reconciliation'
+--  where secondary_database = 'mydb_good_shipped'
 
 
 --4- make sure the others columns in query 2 and 3 have values,if they exist, matching the query 1
