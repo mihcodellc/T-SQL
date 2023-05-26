d45463ee7ca46ba86ed97a0cb355263b17e8490b
monktar <monktar@gmail.com>
2023-05-26 18:07:32 -0500

    Update system-versioned temporal table track.sql

--- system-versioned temporal table track.sql
+++ system-versioned temporal table track.sql
@@ -61,7 +61,7 @@ SELECT @hid 'AS varbinary', @hid.ToString()
 
 
 -- SQL Server Change Tracking
--- https://solutioncenter.apexsql.com/what-is-change-tracking-and-how-to-set-it-up/
+-- https://learn.microsoft.com/en-us/sql/relational-databases/track-changes/work-with-change-tracking-sql-server?view=sql-server-ver16
 -- Enable snapshot isolation
 ALTER DATABASE AdventureWorks
 SET READ_COMMITTED_SNAPSHOT ON
@@ -81,5 +81,30 @@ Select the Change Tracking tab
 ALTER TABLE Person.Address
 ENABLE CHANGE_TRACKING
 WITH (TRACK_COLUMNS_UPDATED = ON)
+declare @synchronization_version bigint;
+-- Obtain the current synchronization version. This will be used next time that changes are obtained.
+SET @synchronization_version = CHANGE_TRACKING_CURRENT_VERSION();
+
+-- Check all tables with change tracking enabled
+IF EXISTS (
+  SELECT 1 FROM sys.change_tracking_tables
+  WHERE min_valid_version > @synchronization_version )
+BEGIN
+  -- Handle invalid version & do not enumerate changes
+  -- Client must be reinitialized
+  print 'hello'
+END
+-- Obtain incremental changes by using the synchronization version obtained the last time the data was synchronized.
+SELECT
+    CT.MyTableID, P.InsCoName,
+    CT.SYS_CHANGE_OPERATION, CT.SYS_CHANGE_COLUMNS,
+    CT.SYS_CHANGE_CONTEXT
+FROM
+    dbo.MyTable AS P
+RIGHT OUTER JOIN
+    CHANGETABLE(CHANGES dbo.LockBoxDocumentAnydoc, @synchronization_version) AS CT
+ON
+    P.MyTableID = CT.MyTableID
+WHERE P.MyTableID in (168979638, 168989653) 
 
 
