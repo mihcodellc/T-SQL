b1533cab13c1973abb16e2b8cc432ab5ef8786c3
monktar <monktar@gmail.com>
2023-06-21 10:35:47 -0500

    Update transact control  error.sql

--- transact control  error.sql
+++ transact control  error.sql
@@ -13,10 +13,21 @@ END TRY
 BEGIN CATCH
     IF @@TRANCOUNT <> 0 
     BEGIN
+	    DECLARE @ErrorMessage NVARCHAR(4000);  
+            DECLARE @ErrorSeverity INT;  
+            DECLARE @ErrorState INT;  
+
+            SELECT @ErrorMessage = ERROR_MESSAGE(),  
+                   @ErrorSeverity = ERROR_SEVERITY(),  
+                   @ErrorState = ERROR_STATE();  
+		--,ERROR_PROCEDURE() AS ErrorProcedure
+                 --,ERROR_LINE() AS ErrorLine
+
 	   -- https://learn.microsoft.com/en-us/sql/t-sql/language-elements/throw-transact-sql?view=sql-server-ver16
 	   --statement before the THROW statement must be followed by the semicolon (;) statement terminator
 	   ROLLBACK TRAN
 	   --RAISERROR ( 'msg', 16, 1 ) WITH LOG; -- Logs the error in the error log and the application log for the instance of the Microsoft SQL Server Database
+           --EXEC xp_logevent 60000, 'msg', informational; -- INFORMATIONAL, WARNING, or ERROR - Logs a user-defined message in the SQL Server log file and in the Windows Event log
 	   THROW; -- re raise error from begin try
 	   RETURN -1
     END
