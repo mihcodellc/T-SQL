dc8338634fb230d7b6a24289dce582c031b59415
monktar <monktar@gmail.com>
2023-09-15 12:15:57 -0500

    Update and rename claim space about shrinking.txt to claim space about shrinking.sql

--- claim space about shrinking.sql
+++ claim space about shrinking.sql
@@ -16,6 +16,14 @@ No other solution found except shrinking the file but
 Shrinking files can lead to serious fragmentation issues at the OS level that are not easily fixed. 
 ref: https://ask.sqlservercentral.com/questions/113796/how-to-reduce-unallocated-space.html
 
+--***Remove the datafile  
+--Empty the file
+DBCC SHRINKFILE (Solutions2, EMPTYFILE);  
+GO 
+--Remove the file
+ALTER DATABASE Solutions REMOVE FILE Solutions2;  
+GO  
+
 other solution maybe #[https://www.brentozar.com/blitz/transaction-log-larger-than-data-file/
 
 https://www.brentozar.com/archive/2009/08/stop-shrinking-your-database-files-seriously-now/
