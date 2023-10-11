87d0b24428d220df523ff0f94844021a9401fc4a
monktar <monktar@gmail.com>
2023-10-11 10:21:37 -0500

    Update Db corruption.sql

--- Db corruption.sql
+++ Db corruption.sql
@@ -1,3 +1,7 @@
 --dbcc checkdb with all_errormsgs, no_infomsgs, tableresults
-
 select * from msdb.dbo.suspect_pages
+  
+-- check important alerts are created. used "Add_important_SQL_Agent _Alerts.sql" to create them
+-- check alert scheduled.
+Select 'SQL Agent Alerts'
+SELECT name, severity, enabled, delay_between_responses, database_name FROM msdb.dbo.sysalerts
