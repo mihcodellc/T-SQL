9314ce22a5fb76f17c1dbfaf28359cee817d2446
monktar <monktar@gmail.com>
2023-11-10 12:33:14 -0600

    Update and rename trace flag- option.sql to trace flag- option restart.sql

--- trace flag- option restart.sql
+++ trace flag- option restart.sql
@@ -2,6 +2,12 @@ trace flag  & query hint
 --https://docs.microsoft.com/en-us/sql/t-sql/database-console-commands/dbcc-traceon-transact-sql?view=sql-server-ver15
 --https://docs.microsoft.com/en-US/sql/t-sql/queries/hints-transact-sql-query?view=sql-server-ver15#examples
 --https://learn.microsoft.com/en-us/sql/t-sql/database-console-commands/dbcc-traceon-trace-flags-transact-sql?view=sql-server-ver15
+
+-- server restart
+  --in cmd promt
+  net start MSSQL$SQLEXPRESS /T902
+  or 
+  -T 902 -- in configuration manager
   
 --Enables the specified trace flags. on command line with -T 
 --OR 
