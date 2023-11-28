ea9fd97724df780879bab54e05bf01adcd3372d9
monktar <monktar@gmail.com>
2023-11-28 12:27:05 -0600

    Update identity column.sql

--- identity column.sql
+++ identity column.sql
@@ -1,11 +1,20 @@
+use MyDB
+
+--check the last id used
+SELECT IDENT_CURRENT('schema.mytable') AS LastIdentityValue;
+
 begin tran
---SET IDENTITY_INSERT mytable ON
-INSERT INTO mytable (
---
-)
+--SET IDENTITY_INSERT schema.mytable ON
+INSERT INTO schema.mytable (col1,col2, ...)
+SELECT val1, val2, ...
 --SET IDENTITY_INSERT mytable OFF
 rollback tran
 
+-- check the insertion
+SELECT * FROM schema.mytable WHERE Ccol1 in (val1)
+
 --check the last id used
-SELECT IDENT_CURRENT('mytable') AS LastIdentityValue;
---DBCC CHECKIDENT ('mytable', RESEED, 38); -- the last used
+SELECT IDENT_CURRENT('schema.mytable') AS LastIdentityValue;
+
+-- reseeed the table to a value
+--DBCC CHECKIDENT ('schema.mytable', RESEED, 38); -- 38 = the last used
