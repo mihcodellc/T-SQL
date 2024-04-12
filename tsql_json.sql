8410c04ce2b9213ad65ebf4b7f2da80ac88853f3
monktar <monktar@gmail.com>
2024-04-12 11:41:28 -0500

    Update tsql_json.sql

--- tsql_json.sql
+++ tsql_json.sql
@@ -7,6 +7,7 @@ if (@compatibility_level >= 130 and @compatibility_level is not null)
 begin
 
 DECLARE @json NVARCHAR(MAX)
+--notice the '[' then '{}' to  put together the many records
 SET @json = '[
 { "id": 1, "name": "John Doe", "email": "johndoe_mail" },
 { "id": 2, "name": "Jane Smith", "email": "janesmith_mail" }
@@ -23,7 +24,9 @@ WITH (id INT, name VARCHAR(50),email VARCHAR(50)
 -- PATH lax or strict for json functions
 -- https://learn.microsoft.com/en-us/sql/relational-databases/json/json-path-expressions-sql-server?view=sql-server-ver16
 declare @jsoninfo nvarchar(max)
-
+-- notice the object
+--    1- "info" with attributs: type, address
+--    2- "type" with attributs: type     
 set @jsoninfo=N'{  
      "info":{    
        "type":1,  
