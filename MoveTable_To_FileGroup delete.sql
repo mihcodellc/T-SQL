dbd803d98bb68b78909b7ddf56cbbbf04f91ae7b
monktar <monktar@gmail.com>
2024-05-31 10:33:09 -0500

    Update MoveTable_To_FileGroup delete.sql

--- MoveTable_To_FileGroup delete.sql
+++ MoveTable_To_FileGroup delete.sql
@@ -60,6 +60,7 @@ GO
 --https://docs.microsoft.com/en-us/sql/t-sql/statements/create-index-transact-sql?view=sql-server-ver16
 --this portion
 --Because the leaf level of a clustered index and the data pages are the same by definition, creating a clustered index and using the ON partition_scheme_name or ON filegroup_name clause effectively moves a table from the filegroup on which the table was created to the new partition scheme or filegroup. Before creating tables or indexes on specific filegroups, verify which filegroups are available and that they have enough empty space for the index.
+--**script indexes.sql knowing the table and filegroup destination	
 CREATE UNIQUE CLUSTERED INDEX PK_Sales_Orders ON Sales.Orders (OrderID)
 	WITH (DROP_EXISTING = ON, ONLINE = ON, FILLFACTOR = 90) ON [FG_Orders]
 
