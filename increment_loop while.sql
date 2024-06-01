20a41c3fba6a4510fbce2c448880b6d916259cef
monktar <monktar@gmail.com>
2024-06-01 14:49:23 -0500

    Update increment_loop while.sql

--- increment_loop while.sql
+++ increment_loop while.sql
@@ -1,3 +1,4 @@
+--forward
 declare @id bigint = 5000
 declare @incr bigint = 5000
 declare @init bigint = 0
@@ -15,5 +16,27 @@ print @id
 
 set @init = @id + 1
 set @id = @id + @incr
+
+end
+
+--backward
+declare @id bigint = 720000
+declare @incr bigint = 5000
+declare @init bigint = @id
+
+set @id = @id - @incr
+
+while @id > 0
+
+begin
+
+
+declare @text varchar(50) = 'between ' + cast(@init as varchar(25)) + ' and ' + cast(@id as varchar(25))
+print @text
+--print @init
+--print @id
+
+set @init = @id - 1
+set @id = @id - @incr
 
 end
