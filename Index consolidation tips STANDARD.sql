0fbf2115ccecbe94e187e33992e7a869de81a15b
monktar <monktar@gmail.com>
2024-08-06 10:58:24 -0500

    Update Index consolidation tips STANDARD.sql

--- Index consolidation tips STANDARD.sql
+++ Index consolidation tips STANDARD.sql
@@ -36,10 +36,11 @@ order by run_datetime desc, index_id desc
 --!!!Important!!!
 --1 create a solution index WITHOUT dropping or modifying (drop_existing=ON) an existing index
 --2 deploying an index if it doesn't work we can drop later it. as long as space is enough to create it
---3 you can remove an index with rollback statement ready; then plan for putting it back, if a db client starts bugging down
+--3 you can drop an index with rollback create statement ready; then plan for putting it back, if a db client starts bugging down
 --4 no more 5 keys columns and 10 in includes
 --5 test your script is error free and a gain in performance before deploying whenever is possible
 --6 consider the cost/size of your new index versus the existing indexes having the columns needed by the query to optimize 
+--7 may need to use hint of new index to see that it performs better then the default cost efefctive chose by the engine; if so, you can drop it but monitoring afterward	
 	
 
 CREATE INDEX [IX_LockboxDocumentTracking_Lbxid_inc_20220427] ON [dbo].[LockboxDocumentTracking] ( [lbxId] ) 
