6f260da225adee8d160bc2306c676d7b55c8ed9c
monktar <monktar@gmail.com>
2024-08-21 09:29:48 -0500

    Update DBCC for a table indexes stats.sql

--- DBCC for a table indexes stats.sql
+++ DBCC for a table indexes stats.sql
@@ -42,3 +42,7 @@ SELECT
 FROM [AdventureWorks2012].[Person].[Person] as p
 JOIN [AdventureWorks2012].[Person].[EmailAddress] as e on p.BusinessEntityID = e.BusinessEntityID
 OPTION (querytraceon 9292,querytraceon 9204,querytraceon 3604)
+
+--Trace Flag 9292 produces a list of statistics that the query optimizer considered. These appear after “Stats header loaded:”
+--Trace Flag 9204 produces a list of statistics that were fully loaded and used by the query optimizer. These appear after “Stats loaded:”.
+--Trace Flag 3604 forces the results of the two previously stated trace flags to appear under the messages tab in SSMS. This should be enabled in addition to the two previously mentioned.
