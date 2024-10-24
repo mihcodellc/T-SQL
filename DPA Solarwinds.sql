0589d61bd854b591eeaaba9fe2f6c622e89df0f2
monktar <monktar@gmail.com>
2024-10-24 17:50:48 -0500

    Update DPA Solarwinds.sql

--- DPA Solarwinds.sql
+++ DPA Solarwinds.sql
@@ -5,4 +5,4 @@
 	tables tunign best practices found when hover summarry on the line of "Current Table Information"
 		what has been done, what left https://documentation.solarwinds.com/en_vers/success_center/dpa/2024-2/content/dpa-table-tuning-best-practices.htm?cshid=bestPracticesTableTuning
 attaches to it
-eg: https://asp-sql003.rms-asp.com:8124/iwc/ng/indexAnalysis?db_id=1&tableFqId=44&date=2024-10-22&sqlHash=2290571483
+
