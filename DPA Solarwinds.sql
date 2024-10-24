05b62673be06b36b6c34d78bf6a17b67bfbade09
monktar <monktar@gmail.com>
2024-10-24 17:49:41 -0500

    Create DPA Solarwinds.sql

---
+++ DPA Solarwinds.sql
@@ -0,0 +1,8 @@
+----DPA has tables advisors plus 
+	indexes, 
+	plans ie queries -- jewel as it has to connect queries to indexes
+	Inefficient SQLs on this table
+	tables tunign best practices found when hover summarry on the line of "Current Table Information"
+		what has been done, what left https://documentation.solarwinds.com/en_vers/success_center/dpa/2024-2/content/dpa-table-tuning-best-practices.htm?cshid=bestPracticesTableTuning
+attaches to it
+eg: https://asp-sql003.rms-asp.com:8124/iwc/ng/indexAnalysis?db_id=1&tableFqId=44&date=2024-10-22&sqlHash=2290571483
