ead55f42e536a5195235de6d91599fed8c52ef3e
monktar <monktar@gmail.com>
2025-04-11 12:01:10 -0500

    Update max_dop.sql

--- max_dop.sql
+++ max_dop.sql
@@ -1,1 +1,2 @@
+https://learn.microsoft.com/en-us/sql/database-engine/configure-windows/configure-the-max-degree-of-parallelism-server-configuration-option?view=sql-server-ver16
 https://www.brentozar.com/archive/2019/12/microsofts-guidance-on-how-to-set-maxdop-has-changed/
